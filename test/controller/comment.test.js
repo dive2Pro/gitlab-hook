b9a29d98bd895fbe7558dce5547c783a28c4b4f7
hyc <huangyichuan@wshifu.com>
2020-11-04 14:02:25 +0800

    add test for controller

--- test/controller/comment.test.js
+++ test/controller/comment.test.js
@@ -2,18 +2,6 @@ const { app, mock, assert } = require('egg-mock/bootstrap');
 const DATA_MERGE_COMMENT = require('./merge_comment.json');
 
 describe('test/controller/comment.test.js', () => {
-    let app;
-    before(() => {
-        app = mock.app();
-        return app.ready();
-    })
-
-    // it('should mock ctx.body.merge_request', () => {
-    //     const ctx = app.mockContext({
-
-    //     })
-    //     assert(ctx)
-    // })
     it('mock comment on merge request ', () => {
         app.mockCsrf();
 
@@ -22,6 +10,6 @@ describe('test/controller/comment.test.js', () => {
             .type('form')
             .send(DATA_MERGE_COMMENT)
             .expect(200)
-            .expect('merge_request')
+            .expect('OK')
     })
 })
---
+++ test/controller/mergeRequest.test.js
@@ -0,0 +1,47 @@
+const {app} = require("egg-mock/bootstrap");
+const DATA_MERGE_CREATE = require("./merge_create.json");
+
+describe("test/controller/mergeRequest.test.js", () => {
+    it("create a log", () => {
+        app.mockCsrf();
+
+        return app
+            .httpRequest()
+            .post("/merge_request")
+            .type("form")
+            .send(DATA_MERGE_CREATE)
+            .expect(200)
+    });
+
+    it("update a log", () => {
+        app.mockCsrf();
+
+        return app
+            .httpRequest()
+            .post("/merge_request")
+            .type("form")
+            .send({...DATA_MERGE_CREATE, object_attributes: {...DATA_MERGE_CREATE.object_attributes, action: 'update'}})
+            .expect(200)
+    });
+
+    it("update a log with review users", () => {
+        app.mockCsrf();
+
+        return app
+            .httpRequest()
+            .post("/merge_request")
+            .type("form")
+            .send({
+                ...DATA_MERGE_CREATE, object_attributes: {
+                    ...DATA_MERGE_CREATE.object_attributes,
+                    description: `closes #13
+                    @jie @chenyiqiu 看看这个
+                    @chenhui
+                    `,
+                    action: 'update'
+                }
+            })
+            .expect(200)
+    });
+
+});
